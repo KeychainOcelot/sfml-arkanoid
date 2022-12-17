1c986ab113619e082538fb05131b42e8e98dafe3
KeychainOcelot <mysticshunal@gmail.com>
2022-12-17 15:16:09 +0530

    added collision

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clang++ -o $1 -std=c++11 -O3 -lsfml-system -lsfml-graphics -lsfml-window -lsfml-audio "./$1.cpp"
+clang++ -o $1 -std=c++20 -O3 -lsfml-system -lsfml-graphics -lsfml-window -lsfml-audio "./$1.cpp"
--- main.cpp
+++ main.cpp
@@ -4,19 +4,16 @@
 using namespace std;
 using namespace sf;
 
-//using 'constexpr' to define immutable compile-time values for resolutions
+//using 'constexpr' to define immutable compile-time values for things
 constexpr int windowWidth{800}, windowHeight{600};
 constexpr float ballRadius{10.f}, ballVelocity{8.f};
+constexpr float paddleWidth{60.f}, paddleHeight{20.f}, paddleVelocity{6.f};
 
-//Class for a ball
-struct Ball //struct because we want the implicitly-public things
+struct Ball
 {
     CircleShape shape; //Create a circle
-
     Vector2f velocity{-ballVelocity, -ballVelocity};
 
-    //Ball constructor
-    //Apply position, radius, color and origin to the shape created from CircleShape
     Ball(float mX, float mY)
     {
         shape.setPosition(mX, mY);
@@ -43,10 +40,57 @@ struct Ball //struct because we want the implicitly-public things
     float bottom() {return y() + shape.getRadius();}
 };
 
+struct Paddle
+{
+    RectangleShape shape;
+    Vector2f velocity;
+
+    Paddle(float mX, float mY)
+    {
+        shape.setPosition(mX, mY);
+        shape.setSize({paddleWidth, paddleHeight});
+        shape.setFillColor(Color::Red);
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+    }
+
+    void update()
+    {
+        shape.move(velocity);
+
+        if(Keyboard::isKeyPressed(Keyboard::Key::Left) && left() > 0) velocity.x = -paddleVelocity;
+        else if(Keyboard::isKeyPressed(Keyboard::Key::Right) && right() < windowWidth) velocity.x = paddleVelocity;
+
+        else velocity.x = 0;
+    }
+
+    float x() {return shape.getPosition().x;}
+    float y() {return shape.getPosition().y;}
+    float left() {return x() - shape.getSize().x / 2.f;}
+    float right() {return x() + shape.getSize().x / 2.f;}
+    float top() {return y() - shape.getSize().y / 2.f;}
+    float bottom() {return y() + shape.getSize().y / 2.f;}
+};
+
+template<class T1, class T2> bool isIntersecting(T1& mA, T2& mB)
+{
+    return mA.right() >= mB.left() && mA.left() <= mB.right() && mA.bottom() >= mB.top() && mA.top() <= mB.bottom();
+}
+
+void testCollision(Paddle& mPaddle, Ball& mBall)
+{
+    if(!isIntersecting(mPaddle, mBall)) return;
+
+    mBall.velocity.y = -ballVelocity;
+
+    if(mBall.x() < mPaddle.x()) mBall.velocity.x = -ballVelocity;
+    else mBall.velocity.x = ballVelocity;
+}
+
 int main()
 {
     //Creation of an instance of a ball
     Ball ball{windowWidth / 2, windowHeight /2};
+    Paddle paddle{windowWidth / 2, windowHeight - 50};
 
     //Creation of game window
     RenderWindow window{{windowWidth, windowHeight}, "Arkanoid"};
@@ -58,11 +102,14 @@ int main()
         //"Clear" the window from previously-drawn graphics
         window.clear(Color::Black);
 
-        //If "escape" is pressed, break out of the loop
+        //If "escape" is pressed, break out of the while loop
         if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
         
-        //Rendering the ball in the window
         ball.update();
+        paddle.update();
+
+        testCollision(paddle, ball);
+
         window.draw(ball.shape);
         window.display();
     }
