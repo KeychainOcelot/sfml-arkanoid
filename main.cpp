b464b9d6c98b11b1d6f3486325d5b67e1e75db8d
KeychainOcelot <mysticshunal@gmail.com>
2022-12-17 14:57:26 +0530

    created borders to bounce the ball

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@ using namespace sf;
 
 //using 'constexpr' to define immutable compile-time values for resolutions
 constexpr int windowWidth{800}, windowHeight{600};
-constexpr float ballRadius{10.f};
+constexpr float ballRadius{10.f}, ballVelocity{8.f};
 
 //Class for a ball
 struct Ball //struct because we want the implicitly-public things
 {
     CircleShape shape; //Create a circle
+
+    Vector2f velocity{-ballVelocity, -ballVelocity};
 
     //Ball constructor
     //Apply position, radius, color and origin to the shape created from CircleShape
@@ -22,6 +24,23 @@ struct Ball //struct because we want the implicitly-public things
         shape.setFillColor(Color::Red);
         shape.setOrigin(ballRadius, ballRadius);
     }
+    
+    void update()
+    {
+        shape.move(velocity);
+
+        if(left() < 0) velocity.x = ballVelocity;
+        else if(right() > windowWidth) velocity.x = -ballVelocity;
+
+        if(top() < 0) velocity.y = ballVelocity;
+        else if(bottom() > windowHeight) velocity.y = -ballVelocity;
+    }
+    float x() {return shape.getPosition().x;}
+    float y() {return shape.getPosition().y;}
+    float left() {return x() - shape.getRadius();}
+    float right() {return x() + shape.getRadius();}
+    float top() {return y() - shape.getRadius();}
+    float bottom() {return y() + shape.getRadius();}
 };
 
 int main()
@@ -30,7 +49,7 @@ int main()
     Ball ball{windowWidth / 2, windowHeight /2};
 
     //Creation of game window
-    RenderWindow window{{windowWidth, windowHeight}, "Arkanoid - 1"};
+    RenderWindow window{{windowWidth, windowHeight}, "Arkanoid"};
     window.setFramerateLimit(60);
 
     //Game loop
@@ -43,6 +62,7 @@ int main()
         if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
         
         //Rendering the ball in the window
+        ball.update();
         window.draw(ball.shape);
         window.display();
     }
